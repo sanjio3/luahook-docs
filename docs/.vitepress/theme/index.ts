100aa433830b6aafcf11cd717ef0b28f98de629a
KuLiPai <yun.svip@gmail.com>
2025-06-01 19:18:28 +0800

    恢复主题，自然飘带

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,8 +1,26 @@
 
 import DefaultTheme from 'vitepress/theme'
+import './custom.css';
 import { h, defineComponent, nextTick, watch } from 'vue'
 import { useRoute } from 'vitepress'
 import confetti from 'canvas-confetti'
+
+
+
+
+var count = 200;
+var defaults = {
+  origin: { y: 0.7 }
+};
+
+function fire(particleRatio, opts) {
+  confetti({
+    ...defaults,
+    ...opts,
+    particleCount: Math.floor(count * particleRatio)
+  });
+}
+
 
 
 
@@ -49,10 +67,27 @@ export default {
           if (!triggered && scrollTop + viewportHeight >= fullHeight - 10) {
             triggered = true
             console.log('[confetti] scroll triggered')
-            confetti({
-              particleCount: 200,
-              spread: 80,
-              origin: { y: 0.8 }
+            fire(0.25, {
+              spread: 26,
+              startVelocity: 55,
+            });
+            fire(0.2, {
+              spread: 60,
+            });
+            fire(0.35, {
+              spread: 100,
+              decay: 0.91,
+              scalar: 0.8
+            });
+            fire(0.1, {
+              spread: 120,
+              startVelocity: 25,
+              decay: 0.92,
+              scalar: 1.2
+            });
+            fire(0.1, {
+              spread: 120,
+              startVelocity: 45,
             })
           }
         })
